c3d5a958cdb3d0fccd82fb24682e78c3045a0151
MarcusChong123 <kok_yung@hotmail.com>
2020-02-17 22:21:18 +0800

    update 20200217_002

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AlertController } from '@ionic/angular';
+import { ChartDataSets } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,58 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  // Data
+  chartData: ChartDataSets[] = [{ data: [], label: 'Stock price' }];
+  chartLabels: Label[];
 
-  constructor() {}
+  // Options
+  chartOptions = {
+    responsive: true,
+    title: {
+      display: true,
+      text: 'Historic Stock price'
+    },
+    pan: {
+      enabled: true,
+      mode: 'xy'
+    },
+    zoom: {
+      enabled: true,
+      mode: 'xy'
+    }
+  };
+  chartColors: Color[] = [
+    {
+      borderColor: '#000000',
+      backgroundColor: '#ff00ff'
+    }
+  ];
+  chartType = 'line';
+  showLegend = false;
+
+  // For search
+  stock = '';
+
+  constructor(private http: HttpClient) {}
+
+  getData() {
+    this.http.get(`https://financialmodelingprep.com/api/v3/historical-price-full/${this.stock}?from=2018-03-12&to=2019-03-12`).
+    subscribe(res => {
+    const history = res['historical'];
+
+    this.chartLabels = [];
+    this.chartData[0].data = [];
+
+    for (let entry of history) {
+      this.chartLabels.push(entry.date);
+      this.chartData[0].data.push(entry['close']);
+    }
+  });
+  }
+
+  typeChanged(e) {
+    const on = e.detail.checked;
+    this.chartType = on ? 'line' : 'bar';
+  }
 
 }
